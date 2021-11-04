fe63967d050604d896e7f3a4416f467c43ac7593
Dmitry <madmassivernb@yandex.ru>
2021-11-05 00:29:07 +0300

    refactoring

--- Scripts/UI/DataForRadar.cs
+++ Scripts/UI/DataForRadar.cs
@@ -3,12 +3,14 @@ using System;
 
 public class DataForRadar : Node
 {
+    private static int unitId = -1;
+
     public Vector2 Position { get; set; } = new Vector2();
     public Vector2 [] EngineTrace {get; private set;} = new Vector2[Game.engineTraceCapacity];
     public Vector2 LabelSize {get; private set;} = new Vector2(3,3);
     public Color Color {get; private set;} = new Color(255,255,255);
-    private static int unitId = -1;
-    private DataForRadar() //конструктор поумолчанию объявлен явно тк моно требовал чтобы он был
+
+    public DataForRadar() //конструктор поумолчанию объявлен явно тк моно требовал чтобы он был
     {}
 
     public DataForRadar(Vector2 position, Vector2[] engineTrace, int engineTraceCapacity, Vector2 labelSize, Color color)
